3f593a2867770d0e78f34061fc334593298f8203
JiaxinWeiZhou <you@example.com>
2024-02-14 10:51:41 +0100

    Servidor

---
+++ src/TCP/Chat.java
@@ -0,0 +1,19 @@
+package TCP;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Chat {
+    public static void main(String[] args) {
+        int puerto = 1234;
+        ServerSocket servidor;
+        try{
+            servidor = new ServerSocket(puerto);
+            System.out.println("Servidor en el puerto: "+ puerto);
+
+            servidor.close();
+        }   catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
