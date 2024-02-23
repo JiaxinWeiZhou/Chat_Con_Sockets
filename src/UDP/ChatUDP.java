49fec19bdbf605f3a2eba2ba90dc0c17ce970bf7
Jiaxin Wei Zhou <weizhoujiaxin@gmail.com>
2024-02-23 15:48:28 +0100

    Change name and finish CHATUDP

---
+++ src/UDP/ChatUDP.java
@@ -0,0 +1,152 @@
+package UDP;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.Serial;
+import java.net.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ChatUDP extends JFrame implements ActionListener, Runnable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    static MulticastSocket ms = null;
+    static InetAddress grupo = null;
+    private static final int puerto = 1111;
+    byte[] buffer = new byte[1024];
+    private JTextArea textAreaTexto;
+    private JTextField mensaje;
+    private JButton btnEnviar;
+    private JButton btnSalir;
+    private JPanel jpanel;
+    String nombre;
+    boolean online = true;
+
+    public ChatUDP(String nombre){
+        super(" VENTANA DE CHAT UDP - Usuario: " + nombre);
+        this.nombre = nombre;
+
+        jpanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10);
+
+        mensaje = new JTextField(20);
+        textAreaTexto = new JTextArea(10, 40);
+        textAreaTexto.setEditable(false);
+        JScrollPane sp = new JScrollPane(textAreaTexto);
+        btnEnviar = new JButton("Enviar");
+        btnSalir = new JButton("Salir");
+
+        //añadir en el panel
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 2;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        jpanel.add(mensaje, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 1;
+        gbc.anchor = GridBagConstraints.WEST;
+        jpanel.add(btnEnviar, gbc);
+
+
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.gridwidth = 2;
+        gbc.fill = GridBagConstraints.BOTH;
+        jpanel.add(sp, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        gbc.anchor = GridBagConstraints.EAST;
+        jpanel.add(btnSalir, gbc);
+
+        btnSalir.addActionListener(this);
+        btnEnviar.addActionListener(this);
+
+        add(jpanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btnEnviar) { // al enviar
+            String texto = nombre + ": "+ mensaje.getText();
+            try {
+                DatagramPacket paquete = new DatagramPacket(texto.getBytes(),texto.length(), grupo, puerto);
+                ms.send(paquete);
+                mensaje.setText("");
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+
+        if (e.getSource() == btnSalir) { // al salir
+            String texto = nombre + " ha salido del chat";
+            try {
+                DatagramPacket paquete = new DatagramPacket(texto.getBytes(),texto.length(), grupo, puerto);
+                ms.send(paquete);
+                ms.close();
+                online = false;
+                System.out.println("Abandona el chat: "+ nombre);
+                System.exit(0);
+
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+
+        }
+    }
+
+    @Override
+    public void run() {
+        while(online){
+            try {
+                DatagramPacket p = new DatagramPacket(buffer, buffer.length);
+                ms.receive(p);
+                String texto = new String(p.getData(), 0, p.getLength());
+                textAreaTexto.append(texto + "\n");
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Servidor en puerto "+ puerto);
+        ms = new MulticastSocket(puerto);
+        grupo = InetAddress.getByName("225.0.0.1");
+        ms.joinGroup(grupo);
+
+        String nombre;
+        do{
+            nombre = JOptionPane.showInputDialog("Introduce tu nombre:");
+            if (nombre == null) {
+                System.exit(0);
+            }
+            if (nombre.trim().isEmpty()) {
+                JOptionPane.showMessageDialog(null, "El nombre está vacío. Inténtalo de nuevo.");
+            }else{
+                break;
+            }
+        }while(true);
+
+        if (!nombre.trim().equals("")) {
+            ChatUDP server = new ChatUDP(nombre);
+            server.setVisible(true);
+            new Thread(server).start();
+
+        } else {
+            System.out.println("El nombre está vacío....");
+        }
+
+    }
+}
